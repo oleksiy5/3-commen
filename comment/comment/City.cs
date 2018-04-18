a2593297ad19e083d23573c47991c390c3b14445
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-04-18 17:12:17 +0200

    command

---
+++ comment/comment/City.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comment
+{
+    class City
+    {
+        string _name;
+
+        public City(string name)
+        {
+            /*
+              Przypisanie nazwy miasta 
+              na zmienną _name
+             */
+            _name = name;
+        }
+
+        /// <summary>
+        /// Pobranie nazwy miasta. Jeśli pusta to zwraca znaki ???
+        /// </summary>
+        /// <returns>nazwa miasta</returns>
+        public string GetName()
+        {
+            if (string.IsNullOrEmpty(_name))
+                return "???";
+            else
+                return _name;
+        }
+    }
+}
